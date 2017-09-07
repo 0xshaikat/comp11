0eb9e08303679b857434c6b0b1a9dbb804743aae
Shaikat Islam <shaikat.islam@tufts.edu>
2017-09-07 00:47:13 -0400

    added labs

---
+++ LAB/LAB0/hello.cpp
@@ -0,0 +1,16 @@
+// Shaikat Islam
+// COMP11
+// Lab 0
+// September 5, 2017
+
+#include <iostream>
+
+using namespace std;
+
+//helloworld
+int main(){
+  cout << "Hello, World!";
+  cout << "\nWassup homies!\n";
+  cout << "the game\n";
+  return 0;
+}
